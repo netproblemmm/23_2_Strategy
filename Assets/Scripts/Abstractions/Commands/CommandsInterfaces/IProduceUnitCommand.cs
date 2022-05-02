382901eaff7564f525d92ade0722cde59b02abec
netproblemmm <ubhawk1@mail.ru>
2022-05-02 17:22:03 +0300

    Lesson_6. Добавил очередь создания юнитов, но она не работает, юриты появляются сразу же по клику. + исправил ошибку в предыдущем коммите.

--- Assets/Scripts/Abstractions/Commands/CommandsInterfaces/IProduceUnitCommand.cs
+++ Assets/Scripts/Abstractions/Commands/CommandsInterfaces/IProduceUnitCommand.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 namespace Abstractions.Commands
 {
-    public interface IProduceUnitCommand : ICommand
+    public interface IProduceUnitCommand : ICommand, IIconHolder
     {
+        float ProductionTime { get; }
         GameObject UnitPrefab { get; }
+        string UnitName { get; }
     }
 }
